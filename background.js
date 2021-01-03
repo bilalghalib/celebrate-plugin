3e855821e38a022e9519ddba9401d56785c11a0d
raghad <raghad.hav@gmail.com>
2021-01-03 20:37:24 +0300

    Adding support for the backend. Style the dialog box.

--- background.js
+++ background.js
@@ -1,13 +1,4 @@
-  //  chrome.webNavigation.onCompleted.addListener(function() {
-  //     alert("This is my favorite website!");
 
-  // }, {url: [{urlMatches : 'http://127.0.0.1:5500/testweb/index.html'}]});
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     //alert(request.message);
-//     // Callback
-//     sendResponse({ message: 'Content script has received that message ⚡' })
-// })
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.cmd === "celebration_file"){
         $.ajax({
@@ -16,15 +7,5 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             success: sendResponse
         });
        return true;
-    } 
-  
-   
+    }    
 })
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        chrome.tabs.query({ active: true }, function(tabs) {
-            const msg = "s";
-            chrome.tabs.sendMessage(tabs[0].id, { "message": msg });
-        })
-    }
-});
--- content.js
+++ content.js
@@ -1,50 +1,86 @@
-$( document ).ready(function() {  
-    chrome.runtime.sendMessage({cmd:"celebration_file"}, function(htmlString){
-        $("body").append(htmlString);
-        let btn = document.getElementById('complete');
-        console.log(btn)
-        if (btn){   
-            btn.addEventListener('click', function() {
-                let anmClass = document.getElementById('animation')
-                let wheelClass=document.getElementById('wheelClass')
-                if (btn.checked){
-                    anmClass.style.display = "block"
-                    // wheelClass.innerHTML="whaaaat"
-                } else {
-                    anmClass.style.display = "none"
-                }
-                
-            })
-        } 
+function loadcelebrations() {
+    fetch('https://raghadhav.com/celebrate/api/celebrations').then(r => r.json()).then(result => {
+        console.log(result)
+        let container = document.getElementById('container');
+        console.log(result.length)
+        for (let i = 0; i < result.length; i++) {
+            let celebrationDiv = document.createElement('div');
+            // protecting from undefined result
+            let name = (result[i].name) ? result[i].name : ''; 
+            let kind = (result[i].kind) ? result[i].kind : ''; 
+            let description = (result[i].description) ? result[i].description : ''; 
+            let serving = (result[i].serving) ? result[i].serving : '';  
+
+            // appending html to div inside the wheel container  
+            let celebrationString = `
+            <name>${name}</name>
+            <kind>${kind}</kind>
+            <description>${description}</description>
+            <serving>${serving}</serving>
+            `
+            celebrationDiv.innerHTML = celebrationString; 
+            container.appendChild(celebrationDiv);
+        }
+    }).then(() => {
         var wheelgame = $('#container').wheelgame({
             shuffle: true,
-            update: function(slice) {
+            update: function (slice) {
                 $('#sel_name').text(slice.name);
                 $('#sel_teakind').text(slice.kind);
                 $('#sel_description').text(slice.description);
                 $('#sel_serving').text(slice.serving);
             },
-            finished: function(slice) {
+            finished: function (slice) {
                 $('#sel_name').text(slice.name);
                 $('#sel_teakind').text(slice.kind);
                 $('#sel_description').text(slice.description);
                 $('#sel_serving').text(slice.serving);
             }
         });
-        $('#spinbtn').on("click",function() {
+        $('#spinbtn').on("click", function () {
             wheelgame.spin();
         });
     })
-    
+}
+
+$(document).ready(function () {
+    chrome.runtime.sendMessage({ cmd: "celebration_file" }, function (htmlString) {
+        // On runtime, append the extention to the body of the website.
+        $("body").append(htmlString);
+        loadcelebrations();
+        let btn = document.getElementById('complete');
+        let doneBtn = document.getElementById('doneBtn');
+        let doneCeleBtn = document.getElementById('doneCeleBtn');
+        let mainContainer = document.getElementById('mainContainer');
+        let closeClass = document.getElementById('closeClass');
+        let anmClass = document.getElementById('animation')
+        let dialogBox = document.getElementById('dialogBox')
+
+        closeClass.addEventListener('click', function () {
+            mainContainer.style.display = "none"
+        })
+        doneCeleBtn.addEventListener('click', function () {
+            mainContainer.style.display = "grid"
+            anmClass.style.display = "none"
+            dialogBox.style.display = "none"
+        })
+        doneBtn.addEventListener('click', function () {
+            dialogBox.style.display = "none"
+            anmClass.style.display = "none"
+        })
+        // if the complete button exists
+        if (btn) {
+            btn.addEventListener('click', function () {
+                // only disply when checked
+                if (btn.checked) {
+                    anmClass.style.display = "block"
+                    dialogBox.style.display = "block"
+                } else {
+                    dialogBox.style.display = "none"
+                }
+
+            })
+        }    
+    })
 })
 
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//     if (request.message == "s") {
-//         var x = document.getElementsByClassName("jss6krjoy");
-// 		x[0].innerHTML = x[0].innerHTML + "<a href='https://bloom.pm/celebrate' id='a' style='z-index:100;  position: relative; target=\"_blank\"'>🎉</a><div id=\"popup\"> </div>"
-//     } 
-//     // Callback
-//     sendResponse({ message: 'Content script has received that message ⚡' })
-// })
-
-        
